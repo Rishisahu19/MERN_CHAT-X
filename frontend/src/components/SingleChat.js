d7a95bed6ba05c4d0aa440c2e9dbe9515c39ec98
Rishisahu19 <rishisahu193@gmail.com>
2024-07-04 19:13:05 +0530

    LINK

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -12,7 +12,7 @@ import io from "socket.io-client";
 import Lottie from "react-lottie";
 import animationData from "../animations/typing.json";
 
-const ENDPOINT = "http://localhost:5000"; // Change this to your deployed endpoint
+const ENDPOINT = "https://mern-chat-x.onrender.com"; // Change this to your deployed endpoint
 let socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
